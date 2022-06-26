223e5bf63332c3212fb23fd240bbc9ce0eecb2f4
Максим Петросян <108141375+Maxim8753@users.noreply.github.com>
2022-06-26 19:23:01 +0300

    Revert "Lesson 1"

--- Практическое задание 2.py
+++ Практическое задание 2.py
@@ -1,6 +1,4 @@
 seconds = int(input('Введите секунды: '))
 minutes = int(seconds/60)
 hour = float(minutes/60)
-result = 'Это {0} часов, {1} минут или {2} секунд'.format(hour,minutes,seconds)
 print('Часов: ', hour, ' Минут: ',minutes, ' Секунд: ',seconds)
-print(result)
