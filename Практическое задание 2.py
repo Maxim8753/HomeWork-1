b4b1f95150492d7e2f9b3bac1dc23e1132fdec90
Максим Петросян <108141375+Maxim8753@users.noreply.github.com>
2022-06-29 00:42:25 +0300

    Update Практическое задание 2.py

--- Практическое задание 2.py
+++ Практическое задание 2.py
@@ -4,3 +4,7 @@ hour = float(minutes/60)
 result = 'Это {0} часов, {1} минут или {2} секунд'.format(hour,minutes,seconds)
 print('Часов: ', hour, ' Минут: ',minutes, ' Секунд: ',seconds)
 print(result)
+hours = int(seconds/3600)
+
+print(f'{hours:02}:{minutes:02}:{seconds:02}')
+
